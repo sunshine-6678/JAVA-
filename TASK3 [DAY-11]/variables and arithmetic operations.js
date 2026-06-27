9f26dec8b4335a7d7ae19e358bb48528123b6cd9
KALPANA_DEVI_V <kalpananivetha395@gmail.com>
2026-06-27 20:33:50 +0530

    Add files via upload

---
+++ TASK3 [DAY-11]/variables and arithmetic operations.js
@@ -0,0 +1,17 @@
+// Variable declaration
+let num1 = 20;
+let num2 = 10;
+
+const message = "Arithmetic Operations";
+
+// Addition
+let addition = num1 + num2;
+
+// Subtraction
+let subtraction = num1 - num2;
+
+// Multiplication
+let multiplication = num1 * num2;
+
+// Division
+let division = num1 / num2;
---
+++ TASK4 [DAY-12]/check whether a number is positive, negative, or zero.js
@@ -0,0 +1,9 @@
+let number = -10;
+
+if (number > 0) {
+    console.log("Positive Number");
+} else if (number < 0) {
+    console.log("Negative Number");
+} else {
+    console.log("Zero");
+}
